153090f41e8bd93de8d7558f5e0688b5522900a6
zubeir-Abubakar <zubkayare@gmail.com>
2019-09-04 10:22:38 +0300

    heroku deployment

--- django_project/settings.py
+++ django_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 MODE = config("MODE", default="dev")
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # email settings
 # EMAIL_USE_TLS = config('EMAIL_USE_TLS')
